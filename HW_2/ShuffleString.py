212d23b1c576797ec966eb44d0d2d49aab5d2b3a
tsirleo <tsirleo@ispras.ru>
2023-09-28 13:03:27 +0300

    ShuffleString.py update

--- HW_2/ShuffleString.py
+++ HW_2/ShuffleString.py
@@ -1,7 +1,6 @@
 data = [input(), input(), "No", []]
 for step in range(len(data[0])):
-    for i in range(step):
-        data[3].append(data[0][i::step])
+    data[3] = [data[0][i::step] for i in range(step)]
     data[2] = step if (data[1] == "".join(data[3]) and str(data[2]) == "No") else data[2]
     data[3].clear()
 print(data[2])
