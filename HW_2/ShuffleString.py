6b7cd51e368b2a8669b624be0d0c3da3791b534d
tsirleo <tsirleo@ispras.ru>
2023-10-20 14:46:40 +0300

    remove old folders

--- HW_2/ShuffleString.py
+++
@@ -1,6 +0,0 @@
-data = [input(), input(), "No", []]
-for step in range(len(data[0])):
-    data[3] = [data[0][i::step] for i in range(step)]
-    data[2] = step if (data[1] == "".join(data[3]) and str(data[2]) == "No") else data[2]
-    data[3].clear()
-print(data[2])
--- HW_3/ShefferStroke.py
+++
@@ -1,8 +0,0 @@
-def sheff(A, B):
-    return False if A and B else A if A else B if B else True
-
-
-# print(sheff(1, 2))
-# print(sheff([], 1.1))
-# print(sheff((0, 0), ""))
-# print(sheff("", []))
